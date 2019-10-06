40c3968ee6b58f6069ef01f1e6f16ae3aee66b2b
Mario Alberto Barragán Espinosa <>
2019-10-06 16:34:38 -0500

    Setting up

---
+++ Project5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Mario Alberto Barragán Espinosa on 10/6/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
